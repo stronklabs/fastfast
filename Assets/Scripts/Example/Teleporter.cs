32298e4d99bda0d0cccd686dd44de95d8554e2df
Yuriy <uvesnin@gmail.com>
2015-08-26 08:22:14 +0300

    Clean Up + Implements CollideHandler
    CollideHandler is the script which helps by customizing calling function
    on enter, exit or stay in editor window.

---
+++ Assets/Scripts/Example/Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UObject = UnityEngine.Object;
+using GObject = UnityEngine.GameObject;
+using URandom = UnityEngine.Random;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.Networking;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Teleporter : ExtBehaviour {
+	void Start () {
+	}
+
+	void Update () {
+		transform.position += new Vector3 (
+			Input.GetAxis ("Horizontal")*0.1f,
+			Input.GetAxis ("Vertical")*0.1f
+		);
+	}
+
+	public void Teleport(GameObject collider) {
+		Debug.Log (collider + " collided with " + this);
+		transform.position = new Vector3 ();
+	}
+}
---
+++ Assets/Scripts/Helpers/CollideHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UObject = UnityEngine.Object;
+using GObject = UnityEngine.GameObject;
+using URandom = UnityEngine.Random;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.Networking;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class CollideHandler : ExtBehaviour {
+	[Serializable]
+	public class Handler : UnityEvent<GameObject> {}
+
+	[FormerlySerializedAs("OnEnter")]
+	[SerializeField]
+	public Handler OnEnter = new Handler();
+	[FormerlySerializedAs("OnExit")]
+	[SerializeField]
+	public Handler OnExit = new Handler();
+	[FormerlySerializedAs("OnStay")]
+	[SerializeField]
+	public Handler OnStay = new Handler();
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		OnEnter.Invoke (other.gameObject);
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		OnExit.Invoke (other.gameObject);
+	}
+
+	void OnTriggerStay2D (Collider2D other) {
+		OnStay.Invoke (other.gameObject);
+	}
+}
